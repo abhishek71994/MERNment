04516d156c7c5cff65fdf9fe212d673d7d379c54
abhishek71994 <abhishek71994@gmail.com>
2018-01-19 01:19:46 +0530

    getting comments from DB

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -1,20 +1,32 @@
 import React,{ Component } from 'react';
 import CommentList from './components/commentlist';
 import CommentForm from './components/commentform';
-import Data from './data';
+import axios from 'axios';
 class CommentBox extends Component{
 	constructor(props){
 		super(props);
 		this.state = {
 			data:[]
 		};
+		this.loadComments = this.loadComments.bind(this);
+	}
+	//render comments from db
+	loadComments(){
+		axios.get(this.props.url).then(
+			res=>{this.setState({data:res.data});}
+		)
+	}
+	//post comments from db
+	componentDidMount(){
+		this.loadComments();
+		setInterval(this.loadComments,this.props.pollInterval);
 	}
 	render(){
 		return(
 			<div>
 				<h2>Comments</h2>
 				<CommentForm/>
-				<CommentList data={Data}/>
+				<CommentList data={this.state.data}/>
 			</div>
 			)
 	}
